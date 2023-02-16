d5b2dbde50be93b2fa344a8b6aa84ff5d94470ba
Lucas Sena <lucassenasilvamachado@gmail.com>
2023-02-16 19:40:46 -0300

    Update script.js

--- 02/script.js
+++ 02/script.js
@@ -2,15 +2,23 @@ var inBusca = document.getElementById("inBusca");
 var btBuscar = document.getElementById("btBuscar");
 var outMsg = document.getElementById("outMsg");
 
-var vetCategorias = ['Ação','Terror','Suspense','Drama','Romance'];
+var vetCategorias = ["Ação", "Terror", "Suspense", "Drama", "Romance"];
 
-function buscar(){
-var busca = inBusca.value;
-for (var i = 0; i < vetCategorias.length; i++) {
-    if(vetCategorias[i] == busca){
-        outMsg.textContent = "A categoria " + vetCategorias[i] + " existe!";
-    }else{
-        outMsg.textContent = "A categoria " + "não existe!";
+function buscar() {
+  var busca = inBusca.value;
+
+  for (var i = 0; i < vetCategorias.length; i++) {
+    if (busca == vetCategorias[i]) {
+      var existe = true;
+      var categoria = busca;
     }
+  }
+  if (existe = true) {
+    outMsg.textContent = "A categoria " + categoria + " existe!";
+    existe = false;
+  } else {
+    outMsg.textContent = "A categoria " + "não existe!";
+    existe = false;
+  }
 }
-}btBuscar.addEventListener("click", buscar);
+btBuscar.addEventListener("click", buscar);
