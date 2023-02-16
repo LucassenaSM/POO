e672fb8ddf047238e63e79a9f1776f0b4dc7aeb7
Lucas Sena <lucassenasilvamachado@gmail.com>
2023-02-16 18:44:08 -0300

    Difícil

--- 03/script.js
+++ 03/script.js
@@ -1,25 +1,54 @@
 var inNumeroUm = document.getElementById("inNumeroUm");
-var inNumeroDois = document.getElementById("inNumeroDois"); 
+var inNumeroDois = document.getElementById("inNumeroDois");
 var btSomar = document.getElementById("btSomar");
+var btSalvar = document.getElementById("btSalvar");
+var btResultado = document.getElementById("btResultado");
 var outMsg = document.getElementById("outMsg");
-  
+
 var vetNumeroUm = [];
 var vetNumeroDois = [];
 var vetSoma = [];
+var soma = 0;
+var numeroUm = 0;
+var numeroDois = 0;
 
-function somar(){   
-    var numeroUm = Number(inNumeroUm.value);
-    var numeroDois = Number(inNumeroDois.value);
-    var soma = numeroUm + numeroDois;
-    console.log(soma);
-    vetSoma.push(soma);
-    vetNumeroUm.push(numeroUm);
-    var saida = "";
-    vetNumeroDois.push(numeroDois); 
-    for (var i = 0; i < vetSoma.length; i++) {
-        
-        saida += " A soma do numero " + vetNumeroUm[i] + " com o numero " + vetNumeroDois[i] + " é " + vetSoma[i] + "\n";
-        outMsg.textContent = saida;
-    }
+function somar() {
+  numeroUm = Number(inNumeroUm.value);
+  numeroDois = Number(inNumeroDois.value);
+  soma = numeroUm + numeroDois;
 
-}btSomar.addEventListener("click", somar)
+  outMsg.textContent = "A soma dos numeros é: " + soma;
+}
+btSomar.addEventListener("click", somar);
+
+function salvar() {
+  vetSoma.push(soma);
+  vetNumeroUm.push(numeroUm);
+  vetNumeroDois.push(numeroDois);
+  var saida = "\n";
+
+  for (var i = 0; i < vetSoma.length; i++) {
+    saida +=
+      " A soma do numero " +
+      vetNumeroUm[i] +
+      " com o numero " +
+      vetNumeroDois[i] +
+      " é " +
+      vetSoma[i] +
+      "\n";
+    outMsg.textContent = saida;
+  }
+}
+btSalvar.addEventListener("click", salvar);
+
+function resultado() {
+  for (var i = 0; i < vetSoma.length; i++) {
+    var somaTotal;
+    somaTotal += vetSoma[i];
+    var saida = "A soma de todas as somas é igual a: " + somaTotal;
+    outMsg.textContent = saida;
+  }
+  vetSoma = [];
+  console.log(vetSoma);
+}
+btResultado.addEventListener("click", resultado);
